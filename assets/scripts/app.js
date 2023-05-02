ebd772c1c15b962efaeedf3a5b27c9c6d6c40b79
TekniqueJoseph <joejacksun@gmail.com>
2023-05-02 18:20:37 -0400

    selector for modal

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,6 @@
+const modal = document.querySelector('.modal')
 const header = document.querySelector('header')
 const adBtn = header.children[1]
-const modal = document.querySelector('.modal')
 
 adBtn.addEventListener ('click', () => {    
     modal.classList.toggle('visible')
